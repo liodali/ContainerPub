eb9da01f3ac5405ba1e5f9992466e7eb8365fba3
liodali <16631886+liodali@users.noreply.github.com>
2025-11-11 01:58:17 +0100

    feat: add local constructor to BaseCommand for testing flexibility

--- dart_cloud_cli/lib/commands/base_command.dart
+++ dart_cloud_cli/lib/commands/base_command.dart
@@ -2,6 +2,7 @@ import 'package:dart_cloud_cli/config/config.dart';
 
 class BaseCommand {
   BaseCommand() : _config = Config();
+  BaseCommand.local() : _config = Config();
   final Config _config;
 
   Config get config => _config;
