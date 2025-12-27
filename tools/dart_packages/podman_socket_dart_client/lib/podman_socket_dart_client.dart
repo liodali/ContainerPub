3fd30c2b09d9e9a6183e2c333b8c047a3b038340
liodali <16631886+liodali@users.noreply.github.com>
2025-12-27 16:08:23 +0100

    feat(podman_socket_dart_client): export compat container config, container operations, and socket client classes
    - Add export for CompatContainerConfig model
    - Add export for ContainerOperations API class
    - Add export for PodmanSocketClient
    - Remove TODO comment

--- tools/dart_packages/podman_socket_dart_client/lib/podman_socket_dart_client.dart
+++ tools/dart_packages/podman_socket_dart_client/lib/podman_socket_dart_client.dart
@@ -5,5 +5,6 @@ library;
 
 export 'src/podman_dart_client_base.dart';
 export 'src/models/container_spec.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/models/compat_container_config.dart';
+export 'src/api/container_operations.dart';
+export 'src/podman_socket_client.dart';
